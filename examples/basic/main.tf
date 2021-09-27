c68f3ce6967cdbe83e5efe27b4cc778bae12fbeb
tmknom <tmk.nom@gmail.com>
2021-09-27 15:16:41 +0900

    specify tag version at source to basic example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,5 +1,5 @@
 module "example" {
-  source = "git@github.com:tmknom/terraform-module-example.git"
+  source = "git@github.com:tmknom/terraform-module-example.git?ref=tags/v0.1.0"
 
   name = "example"
 }
